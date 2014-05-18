585251c96df8ce3149d5c25c916529763c400ed8
Lech Kulina <kulinalech@gmail.com>
2014-05-18 14:55:16 +0200

    Implement signal adapter

--- Include/Common/Common.hpp
+++ Include/Common/Common.hpp
@@ -9,6 +9,47 @@
 #ifndef COMMON_HPP_
 #define COMMON_HPP_
 
+#include <boost/utility.hpp>
+#include <boost/signals2.hpp>
+
 #define COSMIC_UNUSED(x) (void)x
+
+namespace Cosmic {
+
+namespace Common {
+
+    template<class S>
+    class SignalAdapter {
+        public:
+            typedef S Signal;
+            typedef typename Signal::slot_type Slot;
+            typedef boost::signals2::connection Connection;
+
+            explicit SignalAdapter(Signal& signal);
+
+            Connection connect(const Slot& slot);
+            void disconnect(const Slot& slot);
+
+        private:
+            Signal& signal;
+    };
+
+    template<class S>
+    SignalAdapter<S>::SignalAdapter(Signal& signal) :
+        signal(signal) {}
+
+    template<class S>
+    inline typename SignalAdapter<S>::Connection
+    SignalAdapter<S>::connect(const typename SignalAdapter<S>::Slot& slot) {
+        return signal.connect(slot);
+    }
+
+    template<class S>
+    inline void SignalAdapter<S>::disconnect(const Slot& slot) {
+        signal.disconnect(slot);
+    }
+}
+
+}
 
 #endif /* COMMON_HPP_ */
