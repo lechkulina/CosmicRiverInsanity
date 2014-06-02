c1e7f3e1e0db093afa3998fb469daadce673f300
Lech Kulina <kulinalech@gmail.com>
2014-06-03 00:35:26 +0200

    Implement Music as AbstractAsset

--- Include/Core/Music.hpp
+++ Include/Core/Music.hpp
@@ -11,26 +11,52 @@
 
 #include <SDL.h>
 #include <SDL_mixer.h>
+#include <string>
+#include <boost/shared_ptr.hpp>
+#include <boost/weak_ptr.hpp>
+#include <boost/make_shared.hpp>
 #include <boost/filesystem.hpp>
+#include <boost/parameter.hpp>
 #include <Common/Logging.hpp>
+#include <Common/Keywords.hpp>
+#include <Core/AbstractAsset.hpp>
 
 namespace Cosmic {
 
 namespace Core {
 
-    class Music {
+    class Music;
+    typedef boost::shared_ptr<Music> MusicSharedPtr;
+    typedef boost::weak_ptr<Music> MusicWeakPtr;
+
+    class Music : public AbstractAsset {
         public:
-            explicit Music(const boost::filesystem::path& path);
+            explicit Music(const std::string& name,
+                            const boost::filesystem::path& path);
             virtual ~Music();
 
+            virtual const std::string& getName() const;
+            virtual const boost::filesystem::path& getPath() const;
             virtual bool isLoaded() const;
 
             void play(int loops = 0);
 
             operator Mix_Music*();
 
+            BOOST_PARAMETER_MEMBER_FUNCTION(
+                (MusicSharedPtr), static make, Keywords::Tags,
+                (required
+                    (name, (const std::string&))
+                    (path, (const boost::filesystem::path&))
+                )
+            ) {
+                return boost::make_shared<Music>(name, path);
+            }
+
         private:
             Common::Logger logger;
+            std::string name;
+            boost::filesystem::path path;
             Mix_Music* music;
     };
 
--- Source/Core/Music.cpp
+++ Source/Core/Music.cpp
@@ -8,23 +8,26 @@
 
 #include <Core/Music.hpp>
 
-Cosmic::Core::Music::Music(const boost::filesystem::path& path) :
+Cosmic::Core::Music::Music(const std::string& name,
+                            const boost::filesystem::path& path) :
     logger(
         boost::log::keywords::severity = Common::Severity::Trace,
         boost::log::keywords::channel = Common::Channel::Resources),
+    name(name),
+    path(path),
     music(nullptr) {
     BOOST_LOG_FUNCTION();
 
     //load music from file
-    BOOST_LOG(logger) << "Loading music " << path.string();
+    BOOST_LOG(logger) << "Loading music " << name << " from " << path.string();
     if ((music = Mix_LoadMUS(path.string().c_str())) == nullptr) {
         BOOST_LOG_SEV(logger, Common::Severity::Error)
-            << "Failed to load music " << path.string() << ". " << Mix_GetError();
+            << "Failed to load music " << name << " from " << path.string() << ". " << Mix_GetError();
         return;
     }
 
     BOOST_LOG_SEV(logger, Common::Severity::Debug)
-        << "Music " << path.string() << " loaded.";
+        << "Music " << name << " from " << path.string() << " is loaded.";
 }
 
 Cosmic::Core::Music::~Music() {
@@ -38,6 +41,14 @@ Cosmic::Core::Music::~Music() {
     //explicitly halt music before freeing it - we do not want to block here
     Mix_HaltMusic();
     Mix_FreeMusic(music);
+}
+
+const std::string& Cosmic::Core::Music::getName() const {
+    return name;
+}
+
+const boost::filesystem::path& Cosmic::Core::Music::getPath() const {
+    return path;
 }
 
 bool Cosmic::Core::Music::isLoaded() const {
